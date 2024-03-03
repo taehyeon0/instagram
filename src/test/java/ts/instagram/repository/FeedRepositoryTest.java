522c9210c62bcc4c1f5e2d98e636fe7f7fe11fb7
taehyeon0 <taehyeon0.an@gmail.com>
2024-03-03 23:57:18 +0900

    #13 - FeedRepositoryTest 에서 Select, Insert 테스트 완성
    MyBatis 를 사용하여 Select, Insert 동작을 확인하기 위한 테스트 완성함

--- src/test/java/ts/instagram/repository/FeedRepositoryTest.java
+++ src/test/java/ts/instagram/repository/FeedRepositoryTest.java
@@ -8,10 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import ts.instagram.domain.Feed;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 
-@DisplayName("ByBatis 연결 테스트")
+@DisplayName("MyBatis 연결 테스트")
 @Slf4j
 @Transactional
 @SpringBootTest
@@ -20,10 +24,31 @@ class FeedRepositoryTest {
     @Autowired
     FeedRepository feedRepository;
 
+    @DisplayName("select 테스트")
     @Test
-    void findFeeds() {
-        Feed result = feedRepository.selectAll();
-        assertThat(result.getFeed_id()).isEqualTo(1);
-        log.info("feed={}", result);
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // given
+
+        // when
+        List<Feed> result = feedRepository.selectAll();
+        log.info("result={}", result);
+
+        // then
+        assertThat(result).isNotEmpty();
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // given
+        long previousCount = feedRepository.selectAll().stream().count();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Feed feed = new Feed(2, 0, "abc", LocalDateTime.parse("2021-05-30 23:53:46", formatter), "than", LocalDateTime.parse("2021-05-30 23:53:46", formatter), "than");
+
+        // when
+        feedRepository.save(feed);
+
+        // then
+        assertThat(feedRepository.selectAll().stream().count()).isEqualTo(previousCount + 1);
     }
 }
