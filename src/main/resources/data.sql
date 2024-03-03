0629410c83477a9ef9aef9f8bb710d1f2030be26
taehyeon0 <taehyeon0.an@gmail.com>
2024-03-03 16:15:04 +0900

    #8 - Spring Boot 데이터베이스 초기 설정
    데이터베이스 초기 설정을 위해 schema.sql 및 data.sql 추가함

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO FEED (feed_id, content, created_at, created_by, modified_at, modified_by)
+values (1, 'abc', '2021-05-30 23:53:46', 'than', '2021-05-30 23:53:46', 'than');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS feed;
+
+CREATE TABLE feed
+(
+    feed_id INT,
+    content varchar(2200),
+    created_at datetime,
+    created_by varchar(100),
+    modified_at datetime,
+    modified_by varchar(100)
+);
