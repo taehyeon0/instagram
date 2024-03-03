a24076a1edef63d15ba34ede9f84a61472d66d40
taehyeon0 <taehyeon0.an@gmail.com>
2024-03-03 23:52:38 +0900

    #13 - Feed 도메인 수정
    MyBatis 프로퍼티 'map-underscore-to-camel-case' 가 추가되어
    Feed 도메인을 camel case 로 변경함
    추가로 schema.sql 에서 feed 테이블 수정함

--- src/main/java/ts/instagram/domain/Feed.java
+++ src/main/java/ts/instagram/domain/Feed.java
@@ -6,11 +6,21 @@ import java.time.LocalDateTime;
 
 @Data
 public class Feed {
-    private int feed_id;
-    private int user_id;
+    private int feedId;
+    private int userId;
     private String content;
-    private LocalDateTime created_at;
-    private String created_by;
-    private LocalDateTime modified_at;
-    private String modified_by;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+    public Feed(int feedId, int userId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        this.feedId = feedId;
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.modifiedAt = modifiedAt;
+        this.modifiedBy = modifiedBy;
+    }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS feed;
 CREATE TABLE feed
 (
     feed_id INT,
+    user_id INT,
     content varchar(2200),
     created_at datetime,
     created_by varchar(100),
