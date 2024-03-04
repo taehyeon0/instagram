ef68df0c77c2dac4b31cffb1fad2ab01ca8d6ad7
taehyeon0 <taehyeon0.an@gmail.com>
2024-03-04 20:14:48 +0900

    #13 - FeedRepositoryTest 에서 Update, Delete 테스트 완성
    MyBatis 를 사용하여 Update, Delete 동작을 확인하기 위한 테스트 완성함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO FEED (feed_id, content, created_at, created_by, modified_at, modified_by)
-values (1, 'abc', '2021-05-30 23:53:46', 'than', '2021-05-30 23:53:46', 'than');
+INSERT INTO FEED (feed_id, user_id, content, created_at, created_by, modified_at, modified_by)
+values (1, 1, 'abc', '2021-05-30 23:53:46', 'than', '2021-05-30 23:53:46', 'than');
--- src/test/java/ts/instagram/repository/FeedRepositoryTest.java
+++ src/test/java/ts/instagram/repository/FeedRepositoryTest.java
@@ -51,4 +51,34 @@ class FeedRepositoryTest {
         // then
         assertThat(feedRepository.selectAll().stream().count()).isEqualTo(previousCount + 1);
     }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // given
+        Feed feed = feedRepository.selectById(1L);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Feed updatedFeed = new Feed(1, 1, "abc", LocalDateTime.parse("2021-05-30 23:53:46", formatter), "than", LocalDateTime.parse("2021-05-30 23:55:46", formatter), "than");
+
+        // when
+        feedRepository.update(updatedFeed);
+
+        // then
+        assertThat(feedRepository.selectById(1).getModifiedAt()).isEqualTo(updatedFeed.getModifiedAt());
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // given
+        Feed feed = feedRepository.selectById(1L);
+        log.info("feed={}", feed);
+        long previousCount = feedRepository.selectAll().stream().count();
+
+        // when
+        feedRepository.delete(1L);
+
+        // then
+        assertThat(feedRepository.selectAll().stream().count()).isEqualTo(previousCount - 1);
+    }
 }
