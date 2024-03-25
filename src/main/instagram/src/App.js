4986183596e8b54f39f0740e1b30693665621532
DESKTOP-3HJ93DT\User <ssayho09@gmail.com>
2024-03-25 22:21:00 +0900

    #16 리액트 연동
    app.js에 작성하면 됨

---
+++ src/main/instagram/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <header></header>
+    );
+    return (
+        <body>
+        
+        </body>
+    );
+}
+
+export default App;
