652df5c1a9c9071034835feacdb36ebb6a78a6a3
seho Kim <ssayho09@gmail.com>
2025-07-29 00:14:23 +0900

    first commit

--- src/main/instagram/src/App.js
+++ src/main/instagram/src/App.js
@@ -1,37 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Header from './header'
+import Feed from './pages/Feed'
+import Login from './pages/User';
 
-function App() {
-    return (
-        <main className="main">
-            <article className="article">
-                <div className="img">
-                    <h1>여기가 이미지칸</h1>
-                </div>
-                <div className="login-wrap">
-                    <h1>여기가 로그인창</h1>
-                    <div className="login">
-                        <div className="instagram-logo">
-                            <h1>INSTAGRAM</h1>
-                        </div>
-                        <input className="user-id" placeholder="아이디"></input>
-                        <input className="user-password" placeholder="비밀번호"></input>
-                        <div className="login-button">로그인</div>
-                        <div className="or-div">
-                            <div>----</div>
-                            <div>또는</div>
-                            <div>----</div>
-                        </div>
-                        <div className="face-book-login">페이스북로그인</div>
-                        <div className="password-finding">비밀번호를 잊으셨나요?</div>
-                    </div>
-                    <div className="sign-up-wrap">계정이 업으신가요? 가입하기</div>
-                    <div>앱을 다운로드 하세요.</div>
 
-                </div>
-            </article>
-        </main>
+
+
+/*function App() {
+    return (`
+        <div>
+            <Container>
+                <Route path="/" exact{true} Component={Home}></Route>
+                <Route path="/login" exact{true} Component={Login}></Route>
+                <Route path="/feed" exact{true} Component={Feed}></Route>
+            </Container>
+        </div>
+    );
+}*/
+
+const App = () => {
+    return(
+        <div>
+            <Feed/>
+            <Login/>
+        </div>
     );
 }
-
-export default App;
+export default App
--- src/main/java/ts/instagram/config/MyBatisConfig.java
+++ src/main/java/ts/instagram/config/MyBatisConfig.java
@@ -27,4 +27,5 @@ public class MyBatisConfig {
         return new InstauserRepository(instauserMapper);
     }
 
+
 }
